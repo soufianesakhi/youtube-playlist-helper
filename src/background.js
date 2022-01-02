ddad5cef59620f44e10b219846336e465303a479
Soufiane Sakhi <soufiane.sakhi.js@gmail.com>
2022-01-02 20:17:59 +0100

    v2.6.2: Fix playlist builder context menu on Firefox

--- src/background.js
+++ src/background.js
@@ -1,52 +1,52 @@
 const playlistBuilderId = "yphPlaylistBuilder";
 const playlistBuilderPageId = "yphPlaylistBuilderPage";
 
-browser.runtime.onInstalled.addListener(function () {
-  browser.contextMenus.create({
-    id: playlistBuilderId,
-    title: "Add video to the playlist builder",
-    contexts: ["link", "video"]
-  });
-  browser.contextMenus.create({
-    id: playlistBuilderPageId,
-    title: "Add video to the playlist builder",
-    contexts: ["page"],
-    documentUrlPatterns: ["https://www.youtube.com/watch*"]
-  });
+browser.contextMenus.create({
+  id: playlistBuilderId,
+  title: "Add video to the playlist builder",
+  contexts: ["link", "video"],
+});
+browser.contextMenus.create({
+  id: playlistBuilderPageId,
+  title: "Add video to the playlist builder",
+  contexts: ["page"],
+  documentUrlPatterns: ["https://www.youtube.com/watch*"],
 });
 
 browser.contextMenus.onClicked.addListener(async (info, tab) => {
-  if ([playlistBuilderPageId, playlistBuilderId].indexOf("" + info.menuItemId) > -1) {
+  if (
+    [playlistBuilderPageId, playlistBuilderId].indexOf("" + info.menuItemId) >
+    -1
+  ) {
     const link = info.linkUrl || info.pageUrl;
-    if (!link) {
-      console.log("Could not find YouTube video link:", info);
+    if (!link || !(await addLinkToPlaylistBuilder(link))) {
+      alert("Invalid YouTube video link: " + link, true);
       return;
     }
-    addLinkToPlaylistBuilder(link);
     const builderTabs = await openPlaylistBuilderTab();
-    builderTabs.forEach(tab => browser.tabs.reload(tab.id));
+    builderTabs.forEach((tab) => browser.tabs.reload(tab.id));
   }
 });
 
-browser.runtime.onMessage.addListener(async request => {
+browser.runtime.onMessage.addListener(async (request) => {
   if (request.cmd === "get-playlist-builder") {
     return await fetchBuilder();
   } else if (request.cmd === "clear-playlist-builder") {
     await saveBuilder([]);
-    browser.browserAction.setBadgeText({ text: "" })
+    browser.browserAction.setBadgeText({ text: "" });
     return true;
   } else if (request.cmd === "update-playlist-builder") {
     const playlistBuilder = request.playlistBuilder;
     await saveBuilder(playlistBuilder);
-    browser.browserAction.setBadgeText({ text: "" + playlistBuilder.length })
+    browser.browserAction.setBadgeText({ text: "" + playlistBuilder.length });
     return true;
   } else if (request.cmd === "focus-playlist-builder") {
     const builderTabs = await openPlaylistBuilderTab();
     if (builderTabs.length > 0) {
       const windowId = builderTabs[0].windowId;
-      windowId && await browser.windows.update(windowId, { focused: true });
+      windowId && (await browser.windows.update(windowId, { focused: true }));
       const tabId = builderTabs[0].id;
-      tabId && await browser.tabs.update(tabId, { active: true });
+      tabId && (await browser.tabs.update(tabId, { active: true }));
       return true;
     }
   }
@@ -66,7 +66,7 @@ async function saveBuilder(playlistBuilder) {
   browser.storage.local.set(items);
 }
 
-fetchBuilder().then(playlistBuilder => {
+fetchBuilder().then((playlistBuilder) => {
   if (playlistBuilder.length > 0) {
     browser.browserAction.setBadgeText({ text: "" + playlistBuilder.length });
   }
@@ -79,23 +79,36 @@ async function addLinkToPlaylistBuilder(link) {
     playlistBuilder.push(id);
     browser.browserAction.setBadgeText({ text: "" + playlistBuilder.length });
     saveBuilder(playlistBuilder);
+    return true;
   }
+  return false;
 }
 
 async function openPlaylistBuilderTab() {
   const tabs = await browser.tabs.query({
-    url: browser.runtime.getURL(
-      `/editor/index.html`
-    )
+    url: browser.runtime.getURL(`/editor/index.html`),
   });
-  const builderTabs = tabs.filter(tab => tab.url && new URL(tab.url).hash === "#/playlist-builder");
+  const builderTabs = tabs.filter(
+    (tab) => tab.url && new URL(tab.url).hash === "#/playlist-builder"
+  );
   if (builderTabs.length == 0) {
     await browser.tabs.create({
-      url: browser.runtime.getURL(
-        `/editor/index.html#/playlist-builder`
-      ),
+      url: browser.runtime.getURL(`/editor/index.html#/playlist-builder`),
     });
     return [];
   }
   return builderTabs;
+}
+
+/**
+ * @param {string} message
+ * @param {boolean=} isInfo
+ */
+async function alert(message, isInfo) {
+  browser.notifications.create({
+    type: "basic",
+    title: "YouTube Playlist Helper" + (isInfo ? "" : ": Error"),
+    message: message,
+    iconUrl: "icons/icon_48.png",
+  });
 }
