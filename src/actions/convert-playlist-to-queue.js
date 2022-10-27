afb85ae6c13e47d5706b8a2e1c99214169ee6a18
Soufiane Sakhi <soufiane.sakhi.js@gmail.com>
2022-10-27 21:21:14 +0200

    Fix convert playlist to queue

--- src/actions/convert-playlist-to-queue.js
+++ src/actions/convert-playlist-to-queue.js
@@ -1,14 +1,38 @@
 // @ts-nocheck
-document.querySelector('ytd-compact-video-renderer #menu yt-icon').click();
-setTimeout(() => {
-    document.querySelector('ytd-popup-container #items yt-formatted-string').click();
-    setTimeout(() => {
-        const menuBtn = Array.from(document.querySelectorAll('#content ytd-playlist-panel-video-renderer ytd-menu-renderer yt-icon')).pop();
-        menuBtn.scrollIntoView();
-        menuBtn.click();
-        setTimeout(() => {
-            Array.from(document.querySelectorAll("ytd-popup-container #items yt-formatted-string")).pop().click();
-            document.body.scrollIntoView();
-        }, 200);
-    }, 1000);
-}, 200);
+(async () => {
+  async function wait(ms) {
+    return new Promise((r) => setTimeout(r, ms));
+  }
+
+  function lastSelector(selector) {
+    return Array.from(document.querySelectorAll(selector)).pop();
+  }
+
+  for (let i = 0; i < 2; i++) {
+    document.querySelector("ytd-compact-video-renderer #menu yt-icon").click();
+    await wait(200);
+    document
+      .querySelector("ytd-popup-container #items yt-formatted-string")
+      .click();
+  }
+
+  await wait(1000);
+
+  for (let i = 0; i < 2; i++) {
+    await wait(200);
+
+    lastSelector(
+      "#content ytd-playlist-panel-video-renderer ytd-menu-renderer yt-icon"
+    ).click();
+
+    await wait(200);
+
+    lastSelector("ytd-popup-container #items yt-formatted-string").click();
+  }
+
+  document.body.scrollIntoView();
+  
+  await wait(200);
+  
+  alert("Playlist converted to queue");
+})();
